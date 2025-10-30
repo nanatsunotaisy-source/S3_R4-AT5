22103cf6a635ecc87742fff17de3ea58c039580d
unknown <nanatsunotaisy@gmail.com>
2025-10-29 21:41:57 -0300

    Exercicio

---
+++ app.js
@@ -0,0 +1,37 @@
+const { error } = require("console");
+const express = require ("express");
+const app = express();
+const PORT = 8081;
+const fs = require("fs");
+const pathFile = "./livros.json"
+app.use (express.json());
+
+app.post("/livros", (req, res)=>{
+    try {
+        const {titulo, autor, ano, estoque} = req.body;
+
+         const livro = {
+            titulo: titulo,
+            autor: autor,
+            ano: ano,
+            estoque: estoque
+         };
+
+         const data = fs.readFileSync(pathFile, "utf-8");
+         const livros = JSON.parse(data);
+
+         livros.push(livro);
+
+         fs.writeFileSync(pathFile, JSON.stringify(livros, null, 4));
+
+         res.status(201).json({message: "livro cadastrado com sucesso"});
+
+    } catch (error) {
+        console.error("erro ao cadastrar livro:",error);
+        res.status(500).json({error: "erro interno no servidor ao cadastrar livro!"});
+    }
+});
+
+app.listen(PORT, ()=>{
+console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
